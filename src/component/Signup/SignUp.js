86e1af3fc25fdb2d31420ffc60bcb66e21bc2c1a
Kristen Hallstrom <klhalls89@gmail.com>
2019-05-08 20:15:53 -0600

    Begin sign up functionality.
    Co-authored-by:Niraj<niroz.aryal11@gmail.com>

--- src/component/Signup/SignUp.js
+++ src/component/Signup/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { fetchData } from '../../utils/Api'
 
 class SignUp extends Component {
   constructor(){
@@ -10,14 +11,58 @@ class SignUp extends Component {
     }
   }
 
+  //make sure all of the feilds have been filled out before allowing the submit
+  // /api/users/new
+  handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const data = this.state
+    console.log(data)
+    const url = 'http://localhost:3000/api/users/new'
+    const options = {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }
+
+
+      const response = await fetchData(url, options)
+      console.log(response)
+
+    
+  }
+
+  //handle change
+  handleChange = (e) => {
+    const {name, value} = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
   render(){
     return(
       <div>
-        <form>
-          <input type="text" placeholder="Name" name="name" value={this.state.name} onChange={this.handleChange}/>
-          <input type="email" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange}/>
-          <input type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange}/>
-          <button>submit</button>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" 
+                placeholder="Name" 
+                name="name" 
+                value={this.state.name} 
+                onChange={this.handleChange}/>
+
+          <input type="email" 
+                placeholder="Email" name="email" 
+                value={this.state.email} 
+                onChange={this.handleChange}/>
+
+          <input type="password" 
+                placeholder="Password" 
+                name="password" 
+                value={this.state.password} 
+                onChange={this.handleChange}/>
+          <button onSubmit={this.handleSubmit}>submit</button>
         </form>
       </div>
     )
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
-export const fetchData = async (url) => {
+export const fetchData = async (url, options) => {
   try{
-    const response = await fetch(url);
+    const response = await fetch(url, options);
     return await response.json();
     
   }catch(error){
