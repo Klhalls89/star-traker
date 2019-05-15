01e917285df6cbdc3e9009737713cb5792860718
Kristen Hallstrom <klhalls89@gmail.com>
2019-05-14 18:19:02 -0600

    Delete favorite from movies
    Co-authored-by:Niraj<niroz.aryal11@gmail.com>

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -3,6 +3,8 @@ import { addFavoriteMovie } from '../../thunks/addFavorites'
 import { hasErrored, deleteFavorite } from '../../actions'
 import { connect } from 'react-redux'
 import { Redirect} from 'react-router-dom'
+import { fetchData } from '../../utils/Api'
+import { createOption } from '../../utils/options'
 
 export class Card extends Component {
   constructor(){
@@ -13,20 +15,19 @@ export class Card extends Component {
   }
 
   //when user clicks check if user is signed in  
-      // function validate 
+  // function validate 
   // if not signed in display error 
   // if signed in validate if movie is in state tree 
   // if movie is in state remove that movie 
   // if not add to movie 
 
-  // checkForUser = () => {
-  //   console.log(this.props.movie)
-  //   if(typeof this.props.user.id !== 'number'){
-  //     this.setState({ error: "please sign in"})
-  //    }else {
-
-  //    }
-  // }
+  checkForUser = () => {
+    if(typeof this.props.user.id !== 'number'){
+      this.setState({ error: "please sign in"})
+     } else {
+      this.validateMovie()
+     }
+  }
 
   validateMovie = () => {
     const { id } = this.props 
@@ -34,20 +35,22 @@ export class Card extends Component {
     const foundMovie = favorites.find((movie) => {
       return movie.movie_id === id
     })
-   foundMovie ? this.deleteFavorite() : this.addMovie()
-
- 
+    console.log(foundMovie)
+   foundMovie ? this.deleteFavorite(id) : this.addMovie() 
   }
 
-  deleteFavorite = () => {
-    console.log("I am deleting the movie")
+  deleteFavorite = async (id) => {
+    console.log(id)
+    const user_id = this.props.user.id
+    console.log(user_id, "delete user id")
+    const url = `http://localhost:3000/api/users/${user_id}/favorites/${id}`
+    const options = createOption("DELETE")
+    const response = await fetchData(url, options)
+    console.log(response)
   }
   
-
   addMovie = async() =>  {
-   if(typeof this.props.user.id !== 'number'){
-    this.setState({ error: "please sign in"})
-   }else {
+  
     const { addFavoriteMovie } = this.props
     const method = "POST"
     const url ="http://localhost:3000/api/users/favorites/new"
@@ -63,8 +66,7 @@ export class Card extends Component {
     }
     const userId = data.user_id
     addFavoriteMovie(url,method,data,userId) 
-  }
-    
+  
   } 
 
 
@@ -76,7 +78,7 @@ export class Card extends Component {
     <div className="Card">
       <section>
       <p>{vote_average}/10</p>
-      <button onClick={this.validateMovie}><i class="far fa-star"></i></button>
+      <button onClick={this.checkForUser}><i class="far fa-star"></i></button>
       </section>
       <img src={imageUrl}/>
       {this.state.error && this.state.error}
